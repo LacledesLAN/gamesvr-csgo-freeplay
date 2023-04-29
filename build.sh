3b02a5754880bdea62b03b166bd4eeebac34cc01
James Dudley <jedudley@outlook.com>
2023-04-28 21:39:38 -0500

    meta updates

--- build.sh
+++ build.sh
@@ -1,14 +1,10 @@
 #!/bin/bash
-set -e
-
-
-echo -e '\n\033[1m[Preflight Checks]\033[0m'
-echo -e "Docker client version: '$(docker version --format '{{.Client.Version}}')'"
-echo -e "Docker server version: '$(docker version --format '{{.Server.Version}}')'"
+set -e;
+set -u;
 
 
 echo -e '\n\033[1m[Build Image]\033[0m'
-docker build . -f linux.Dockerfile --rm -t lacledeslan/gamesvr-csgo-freeplay:latest --no-cache --pull --build-arg BUILDNODE=$(cat /proc/sys/kernel/hostname);
+docker build . -f linux.Dockerfile --rm -t lacledeslan/gamesvr-csgo-freeplay:latest --no-cache --pull --build-arg BUILDNODE="$(cat /proc/sys/kernel/hostname)";
 
 
 echo -e '\n\033[1m[Running Self-Checks]\033[0m'
